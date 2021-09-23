c68fd5f73edcd5a8881523969a0590d8c16d0d32
Bruno Severino <bo.severino@gmail.com>
2021-09-23 15:41:06 -0300

    config

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import "./styles.css"; // import the tailwind styles
+import { LitElement, customElement, html } from "lit-element";
+
+@customElement("app-component")
+export default class AppComponent extends LitElement {
+  override createRenderRoot() {
+    return this; // turn off shadow dom to access external styles
+  }
+
+  override render() {
+    return html`
+      <h1 class="mx-auto my-4 py-4 text-center shadow-lg text-xl w-1/2">
+        Hello, World!
+      </h1>
+    `;
+  }
+}
